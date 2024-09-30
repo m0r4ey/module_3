299df8539f741f56a8d860abdacd74567ff22908
m0rfey <plaksinaleks2306@mail.ru>
2024-09-30 12:07:54 +0500

    Рекурсия.

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,14 @@
 # Рекурсия.
 def get_multiplied_digits (number):
+    if number == 0:
+        number = 1
     str_number = str(number)
     first = int(str_number[0:1])
     if not (len(str_number) > 1):
         return first
     return first * get_multiplied_digits(int(str_number[1:]))
 
-result = get_multiplied_digits(40203)
-print(result)
+result1 = get_multiplied_digits(40203)
+result2 = get_multiplied_digits(4020)
+print(result1)
+print(result2)
